eb56edca386c8f558a0209d51ab7167080d2e54c
Rico <wolbereric@yahoo.fr>
2019-01-29 18:01:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -67,15 +67,15 @@ app.use(mountPath, api);
 app.use('/dashboard', dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
-});
+// app.get('/', function(req, res) {
+//   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+// });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+// app.get('/test', function(req, res) {
+//     res.sendFile(path.join(__dirname, '/public/test.html'));
+// });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
