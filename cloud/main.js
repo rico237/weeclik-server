1253ee23a4be44dd7b3b9ea0110847daf05c053a
Rico237 <wolbereric@gmail.com>
2019-04-03 23:13:30 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,28 +1,28 @@
-Parse.Cloud.beforeSave("Commerce", (request, response) => {
-  	const description = request.object.get("description");
-	var bannedWords = [
-		"au", "un", "une", "à", "il", "elle", "mais", "où", "est", "donc", "or", "ni", "car", " ",
-		"de", "la", "et", "du", "aux", "le", "se", "fait", "avec", "en", "des", "pas", "deux", 
-	];
+// Parse.Cloud.beforeSave("Commerce", (request, response) => {
+//   	const description = request.object.get("description");
+// 	var bannedWords = [
+// 		"au", "un", "une", "à", "il", "elle", "mais", "où", "est", "donc", "or", "ni", "car", " ",
+// 		"de", "la", "et", "du", "aux", "le", "se", "fait", "avec", "en", "des", "pas", "deux", 
+// 	];
 
-	var sorted = [];
-	for (var i = 0; i < bannedWords.length; i++) {
-		var filtered = bannedWords[i].toLowerCase();
-	    sorted.push(filtered);
-	}
-	sorted.sort();
+// 	var sorted = [];
+// 	for (var i = 0; i < bannedWords.length; i++) {
+// 		var filtered = bannedWords[i].toLowerCase();
+// 	    sorted.push(filtered);
+// 	}
+// 	sorted.sort();
 
-  	var hashtags = [];
+//   	var hashtags = [];
 
-	let res = description.split(" ");
+// 	let res = description.split(" ");
 
-	for (var i = 0; i < res.length; i++) {
-		let word = res[i].toLowerCase().replace(",","").replace(".", "");
-		if (!sorted.includes(word)) {
-			hashtags.push("#"+word);
-		}
-	}
+// 	for (var i = 0; i < res.length; i++) {
+// 		let word = res[i].toLowerCase().replace(",","").replace(".", "");
+// 		if (!sorted.includes(word)) {
+// 			hashtags.push("#"+word);
+// 		}
+// 	}
 
-  	request.object.set("tags", hashtags);
-  	response.success();
-});
+//   	request.object.set("tags", hashtags);
+//   	response.success();
+// });
