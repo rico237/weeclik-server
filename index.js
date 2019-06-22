236e4d5abf6e4e75026c93aaf728a896e4b34177
Rico237 <wolbereric@gmail.com>
2019-06-22 22:29:19 +0200

    Add bot detection

--- index.js
+++ index.js
@@ -26,96 +26,96 @@ moment().format();
 let databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) { 
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+    console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 let options = { allowInsecureHTTP: true };
 
 let dashboard = new ParseDashboard({
-  "apps": [
-  {
-    "serverURL":  process.env.SERVER_URL,
-    "appId":      process.env.APP_ID,
-    "masterKey":  process.env.MASTER_KEY,
-    "appName":    process.env.APP_NAME,
-    "iconName": "logo.png",
-    "supportedPushLocales": ["fr"]
-  }],
-  "iconsFolder": "icons",
-  "users": [
-  {
-    "user": process.env.ADMIN_USER,
-    "pass": process.env.ADMIN_PASSWORD
-  }]
+    "apps": [
+    {
+        "serverURL":  process.env.SERVER_URL,
+        "appId":      process.env.APP_ID,
+        "masterKey":  process.env.MASTER_KEY,
+        "appName":    process.env.APP_NAME,
+        "iconName": "logo.png",
+        "supportedPushLocales": ["fr"]
+    }],
+    "iconsFolder": "icons",
+    "users": [
+    {
+        "user": process.env.ADMIN_USER,
+        "pass": process.env.ADMIN_PASSWORD
+    }]
 }, options);
 
 let gcsOptions = {
-  "projectId": "weeclik-1517332083996",
-  "keyFilename": resolve(__dirname, "WeeClik-813d8719632d.json"),
-  "bucket": "weeclik-1517332083996.appspot.com",
-  "bucketPrefix": 'baas_files/',
-  "directAccess": false
+    "projectId": "weeclik-1517332083996",
+    "keyFilename": resolve(__dirname, "WeeClik-813d8719632d.json"),
+    "bucket": "weeclik-1517332083996.appspot.com",
+    "bucketPrefix": 'baas_files/',
+    "directAccess": false
 }
 let gcsAdapter = new GCSAdapter(gcsOptions);
 
 console.log(process.env.MASTER_KEY)
 
 let api = new ParseServer({
-  databaseURI:        databaseUri,
-  cloud:              process.env.CLOUD_CODE_MAIN     || __dirname + '/cloud/main.js',
-  appId:              process.env.APP_ID,
-  masterKey:          process.env.MASTER_KEY,
-  filesAdapter:       gcsAdapter,
-  serverURL:          process.env.SERVER_URL,
-  publicServerURL:    process.env.PUBLIC_URL,
-  appName:            process.env.APP_NAME,
-  allowClientClassCreation: true,
-  // Enable email verification
-  // try to use this (avantage langue) // "@ngti/parse-server-mailgun": "^2.4.18",
-  verifyUserEmails: process.env.VERIFY_USER_EMAILS || true,
-  emailAdapter: {
-    module: 'parse-server-mailgun',
-    options: {
-      // The address that your emails come from
-      fromAddress: 'Herrick de l\'équipe Weeclik <contact@herrick-wolber.fr>',
-      // Your domain from mailgun.com
-      domain: process.env.ADAPTER_DOMAIN,
-      // Mailgun host (default: 'api.mailgun.net'). 
-      // When using the EU region, the host should be set to 'api.eu.mailgun.net'
-      host: 'api.eu.mailgun.net',
-      // Your API key from mailgun.com
-      apiKey: process.env.ADAPTER_API_KEY,
-      // The template section
-      templates: {
-        passwordResetEmail: {
-          subject: 'Mot de passe oublié ?',
-          pathPlainText: resolve(__dirname, 'email_templates/password_reset/password_reset_email.txt'),
-          pathHtml: resolve(__dirname, 'email_templates/password_reset/password_reset_email.html'),
-          callback: (user) => { return { 
-            name: user.get('name') 
-          }}
-          // Now you can use {{name}} in your templates
-        },
-        verificationEmail: {
-          subject: 'Bienvenue dans la famille Weeclik',
-          pathPlainText: resolve(__dirname, 'email_templates/verification_email/verification_email.txt'),
-          pathHtml: resolve(__dirname, 'email_templates/verification_email/verification_email.html'),
-          callback: (user) => { return { 
-            name: user.get('name') 
-          }}
-          // Now you can use {{name}} in your templates
-        },
-        customEmailAlert: {
-          subject: 'Message urgent',
-          pathPlainText: resolve(__dirname, 'email_templates/custom_email/custom_email.txt'),
-          pathHtml: resolve(__dirname, 'email_templates/custom_email/custom_email.html'),
-          callback: (user) => { return { 
-            name: user.get('name') 
-          }}
-          // Now you can use {{name}} in your templates
-        }
+    databaseURI:        databaseUri,
+    cloud:              process.env.CLOUD_CODE_MAIN     || __dirname + '/cloud/main.js',
+    appId:              process.env.APP_ID,
+    masterKey:          process.env.MASTER_KEY,
+    filesAdapter:       gcsAdapter,
+    serverURL:          process.env.SERVER_URL,
+    publicServerURL:    process.env.PUBLIC_URL,
+    appName:            process.env.APP_NAME,
+    allowClientClassCreation: true,
+      // Enable email verification
+      // try to use this (avantage langue) // "@ngti/parse-server-mailgun": "^2.4.18",
+      verifyUserEmails: process.env.VERIFY_USER_EMAILS || true,
+      emailAdapter: {
+          module: 'parse-server-mailgun',
+          options: {
+          // The address that your emails come from
+          fromAddress: 'Herrick de l\'équipe Weeclik <contact@herrick-wolber.fr>',
+          // Your domain from mailgun.com
+          domain: process.env.ADAPTER_DOMAIN,
+          // Mailgun host (default: 'api.mailgun.net'). 
+          // When using the EU region, the host should be set to 'api.eu.mailgun.net'
+          host: 'api.eu.mailgun.net',
+          // Your API key from mailgun.com
+          apiKey: process.env.ADAPTER_API_KEY,
+          // The template section
+          templates: {
+              passwordResetEmail: {
+                  subject: 'Mot de passe oublié ?',
+                  pathPlainText: resolve(__dirname, 'email_templates/password_reset/password_reset_email.txt'),
+                  pathHtml: resolve(__dirname, 'email_templates/password_reset/password_reset_email.html'),
+                  callback: (user) => { return { 
+                      name: user.get('name') 
+                  }}
+                  // Now you can use {{name}} in your templates
+              },
+              verificationEmail: {
+                  subject: 'Bienvenue dans la famille Weeclik',
+                  pathPlainText: resolve(__dirname, 'email_templates/verification_email/verification_email.txt'),
+                  pathHtml: resolve(__dirname, 'email_templates/verification_email/verification_email.html'),
+                  callback: (user) => { return { 
+                      name: user.get('name') 
+                  }}
+                  // Now you can use {{name}} in your templates
+              },
+              customEmailAlert: {
+                  subject: 'Message urgent',
+                  pathPlainText: resolve(__dirname, 'email_templates/custom_email/custom_email.txt'),
+                  pathHtml: resolve(__dirname, 'email_templates/custom_email/custom_email.html'),
+                  callback: (user) => { return { 
+                      name: user.get('name') 
+                  }}
+                  // Now you can use {{name}} in your templates
+              }
+          }
       }
-    }
   }
 });
 
@@ -136,15 +136,15 @@ app.use('/dashboard', dashboard);
 let port = process.env.PORT || 1337;
 let httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-  console.log('Running weekclik server on port ' + port);
+    console.log('Running weekclik server on port ' + port);
 });
 
 app.get('/cgu/', (req, res) => {
-  res.status(200).send('cgu')
+    res.status(200).send('cgu')
 })
 
 app.get('/politique-confidentialite/', (req, res) => {
-  res.status(200).send('politique-confidentialite')
+    res.status(200).send('politique-confidentialite')
 })
 
 // Toute les heure à 0 minute
@@ -157,115 +157,122 @@ cron.schedule('0 * * * *', () => {
   console.log(`Function executé à ${moment()}`)
   Parse.Cloud.run('retrieveAllObjects', { object_type: "Commerce", only_objectId: false })
   .then((objects) => {
-    console.log("Successfully retrieved " + objects.length + " commerces.");
-    for (let i = 0; i < objects.length; i++) {
-      let object = objects[i];
-      if (object.get('endSubscription') !== undefined) {
-        if (moment(object.get('endSubscription')).isValid()) {
-          let day =  moment(object.get('endSubscription'))
-          if (moment().isSameOrAfter(day)) {
-            console.log(object.get('nomCommerce') +  ' passed date')
-            object.set("statutCommerce", 0)
-            object.save()
+      console.log("Successfully retrieved " + objects.length + " commerces.");
+      for (let i = 0; i < objects.length; i++) {
+          let object = objects[i];
+          if (object.get('endSubscription') !== undefined) {
+              if (moment(object.get('endSubscription')).isValid()) {
+                  let day =  moment(object.get('endSubscription'))
+                  if (moment().isSameOrAfter(day)) {
+                      console.log(object.get('nomCommerce') +  ' passed date')
+                      object.set("statutCommerce", 0)
+                      object.save()
+                  }
+              }
           }
-        }
       }
-    }
   });
 });
 
 app.post('/send-error-mail', (req, res) => {
-  console.log(req.body);
-  if (req.body.content_message) {
-    const data = {
-      from: 'iOS APP <no-reply@weeclik.com>',
-      to: 'contact@herrick-wolber.fr',
-      subject: 'Error in iOS App',
-      text: req.body.content_message
-    };
+    console.log(req.body);
+    if (req.body.content_message) {
+        const data = {
+            from: 'iOS APP <no-reply@weeclik.com>',
+            to: 'contact@herrick-wolber.fr',
+            subject: 'Error in iOS App',
+            text: req.body.content_message
+        };
 
-    mailgun.messages().send(data, (error, body) => {
-      if (error) {
-        return error
-      } else {
-        return res.status(200).send({
-        success: 'true',
-        message: 'Message envoyé avec succès',
-      });
-      }
-      console.log(body);
-      
-    });
-  } else {
-    // TODO: rajouter un test sur le type d'erreur et retourner le bon type d'erreur
-    return res.status(422).send({
-      success: 'false',
-      message: 'Missing parameter : content_message is undefined',
-    })
-  }
+        mailgun.messages().send(data, (error, body) => {
+            if (error) {
+                return error
+            } else {
+                return res.status(200).send({
+                    success: 'true',
+                    message: 'Message envoyé avec succès',
+                });
+            }
+            console.log(body);
+
+        });
+    } else {
+        // TODO: rajouter un test sur le type d'erreur et retourner le bon type d'erreur
+        return res.status(422).send({
+            success: 'false',
+            message: 'Missing parameter : content_message is undefined',
+        })
+    }
 })
 
 app.get('/valid-email/:email', (req, res) => {
-  console.log(`Test address mail valid : ${req.params.email}`)
-  // TODO: change
-  let validator = require('mailgun-validate-email')(process.env.MAILGUN_PUBKEY)
-  validator(req.params.email, (err, result) => {
-    if(err) {
-      // TODO : Send Mail to admin for errors
-      // email was not valid
-      // TODO: rajouter un test sur le type d'erreur et retourner le bon type d'erreur
-      return res.status(400).send({
-        success: 'false',
-        message: 'Invalid mail or server error. Please contact admin fast.',
-      })
-    } else {
-      // TODO: send admin email for high or medium risk email
-      return res.status(200).send({
-        success: 'true',
-        message: result,
-      });
-    }
+    console.log(`Test address mail valid : ${req.params.email}`)
+      // TODO: change
+      let validator = require('mailgun-validate-email')(process.env.MAILGUN_PUBKEY)
+      validator(req.params.email, (err, result) => {
+          if(err) {
+          // TODO : Send Mail to admin for errors
+          // email was not valid
+          // TODO: rajouter un test sur le type d'erreur et retourner le bon type d'erreur
+          return res.status(400).send({
+              success: 'false',
+              message: 'Invalid mail or server error. Please contact admin fast.',
+          })
+      } else {
+          // TODO: send admin email for high or medium risk email
+          return res.status(200).send({
+              success: 'true',
+              message: result,
+          });
+      }
   })
-});
+  });
 
 app.get('/redirect-to-store', (req, res) => {
-  let md = new MobileDetect(req.headers['user-agent'])
-  // TODO: [1] show a rendered html page for unknown devices
-  // TODO: [2] Add can open url - deep linking / universal links (better for ios)
-  
-  if (md.phone() !== null || md.mobile() !== null) {
-    if (md.os() === 'AndroidOS') {
-      return res.redirect(301, process.env.ANDROID_LINK);
-    } else if (md.os() === 'iOS') {
-      return res.redirect(301, process.env.IOS_LINK);
-    } else {
-      const data = {
-        from: 'Web server <no-reply@weeclik.com>',
-        to: 'contact@herrick-wolber.fr',
-        subject: 'redirect to store problem',
-        text: JSON.stringify(md)
-      };
+    let md = new MobileDetect(req.headers['user-agent'])
+    // TODO: [1] show a rendered html page for unknown devices
+    // TODO: [2] Add can open url - deep linking / universal links (better for ios)
+    // 
+    // https://github.com/mderazon/node-deeplink
+    // https://stackoverflow.com/questions/32689483/ios9-try-to-open-app-via-scheme-if-possible-or-redirect-to-app-store-otherwise/32774701#32774701
+    // https://www.raywenderlich.com/6080-universal-links-make-the-connection
+    
+    // IF NOT A BOT
+    if (!md.is('bot')) {
+        if (md.phone() !== null || md.mobile() !== null) {
+            if (md.os() === 'AndroidOS') {
+                return res.redirect(301, process.env.ANDROID_LINK);
+            } else if (md.os() === 'iOS') {
+                return res.redirect(301, process.env.IOS_LINK);
+            } else {
+                const data = {
+                    from: 'Web server <no-reply@weeclik.com>',
+                    to: 'contact@herrick-wolber.fr',
+                    subject: 'redirect to store problem',
+                    text: JSON.stringify(md)
+                };
 
-      mailgun.messages().send(data, (error, body) => {
-        console.log(body);
-      });
-      // HERE [1] 
-      return res.status(200).send("Appareil iconnu")
-    }
-  } else {
-    const data = {
-      from: 'Web server <no-reply@weeclik.com>',
-      to: 'contact@herrick-wolber.fr',
-      subject: 'redirect to store problem',
-      text: JSON.stringify(md)
-    };
+                mailgun.messages().send(data, (error, body) => {
+                    console.log(body);
+                });
+                // HERE [1] 
+                return res.status(200).send("Appareil iconnu")
+            }
+        } else {
+            const data = {
+                from: 'Web server <no-reply@weeclik.com>',
+                to: 'contact@herrick-wolber.fr',
+                subject: 'redirect to store problem',
+                text: JSON.stringify(md)
+            };
 
-    mailgun.messages().send(data, (error, body) => {
-      console.log(body);
-    });
+            mailgun.messages().send(data, (error, body) => {
+                console.log(body);
+            });
 
-    console.log(JSON.stringify(md))
-    // HERE [1] 
-    return res.status(200).send(`L'appareil n'est pas un téléphone <br><br>${JSON.stringify(md)}`)
-  }
+            console.log(JSON.stringify(md))
+            // HERE [1] 
+            return res.status(200).send(`L'appareil n'est pas un téléphone <br><br>${JSON.stringify(md)}`)
+        }
+    } 
 })
