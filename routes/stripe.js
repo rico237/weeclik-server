e420a9bf4a354fb820ea3d84d6ad43aca44fa724
Rico237 <wolbereric@gmail.com>
2020-11-19 21:14:37 +0100

    Remove un-tested route API

--- routes/stripe.js
+++ routes/stripe.js
@@ -59,40 +59,40 @@ module.exports = function(app){
   });
 
   // Webhook handler for asynchronous events.
-  app.post('/stripe-response-webhook', async (req, res) => {
-    let data;
-    let eventType;
-    // Check if webhook signing is configured.
-    if (process.env.STRIPE_WEBHOOK_SECRET) {
-      // Retrieve the event by verifying the signature using the raw body and secret.
-      let event;
-      let signature = req.headers['stripe-signature'];
+  // app.post('/stripe-response-webhook', async (req, res) => {
+  //   let data;
+  //   let eventType;
+  //   // Check if webhook signing is configured.
+  //   if (process.env.STRIPE_WEBHOOK_SECRET) {
+  //     // Retrieve the event by verifying the signature using the raw body and secret.
+  //     let event;
+  //     let signature = req.headers['stripe-signature'];
 
-      try {
-        event = stripe.webhooks.constructEvent(
-          req.rawBody,
-          signature,
-          process.env.STRIPE_WEBHOOK_SECRET
-        );
-      } catch (err) {
-        console.log(`⚠️  Webhook signature verification failed.`);
-        return res.sendStatus(400);
-      }
-      // Extract the object from the event.
-      data = event.data;
-      eventType = event.type;
-    } else {
-      // Webhook signing is recommended, but if the secret is not configured in `config.js`,
-      // retrieve the event data directly from the request body.
-      data = req.body.data;
-      eventType = req.body.type;
-    }
+  //     try {
+  //       event = stripe.webhooks.constructEvent(
+  //         req.rawBody,
+  //         signature,
+  //         process.env.STRIPE_WEBHOOK_SECRET
+  //       );
+  //     } catch (err) {
+  //       console.log(`⚠️  Webhook signature verification failed.`);
+  //       return res.sendStatus(400);
+  //     }
+  //     // Extract the object from the event.
+  //     data = event.data;
+  //     eventType = event.type;
+  //   } else {
+  //     // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+  //     // retrieve the event data directly from the request body.
+  //     data = req.body.data;
+  //     eventType = req.body.type;
+  //   }
 
-    if (eventType === 'checkout.session.completed') {
-      console.log(`🔔  Payment received!`);
-    }
+  //   if (eventType === 'checkout.session.completed') {
+  //     console.log(`🔔  Payment received!`);
+  //   }
 
-    res.sendStatus(200);
-  });
+  //   res.sendStatus(200);
+  // });
 
 }
